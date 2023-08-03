4748be496fed2e6a79ce3f6793cfa38433298259
Eduardo <eduardo.campos@grupoicts.com.br>
2023-08-03 01:51:26 -0300

    Autenticação de login funcionando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,16 @@
 "use client"
 import RINGS from "vanta/dist/vanta.rings.min"
 import * as THREE from "three"
-import React, { useState, useEffect, useRef } from 'react'
-import {styled, TextField} from "@mui/material";
+import React, {useState, useEffect, useRef, FormEvent} from 'react'
+import {Button, styled, TextField} from "@mui/material";
+import {api} from "@/service/api";
+interface User{
+    email: String,
+    password: String
+}
 
 export default function Login() {
-    const CssTextField = styled(TextField)({
-        '& .MuiInputBase-input': {
-            color: '#A0AAB4',
-        },
-        '& .MuiInputBase:hover': {
-            color: "red"
-        },
-        '& label.Mui-focused': {
-            color: '#A0AAB4',
-        },
-        '& label': {
-            color: '#A0AAB4',
-        },
-        '& .MuiInput-underline:before': {
-            borderBottomColor: '#B2BAC2',
-        },
-        '& .MuiInput-underline:after': {
-            borderBottomColor: '#FFF',
-        },
-    });
+    const [user, setUser] = useState<User>({email: '', password: ''})
     const [vantaEffect, setVantaEffect] = useState(null)
     const myRef = useRef(null)
     useEffect(() => {
@@ -48,19 +34,76 @@ export default function Login() {
             if (vantaEffect) vantaEffect.destroy()
         }
     }, [vantaEffect])
-  return (
-      <div ref={myRef} className="h-[100vh] w-[100vw]">
-          <div className={"flex flex-col justify-center card h-[100vh] w-[25vw] bg-violet-400 bg-opacity-10 backdrop-blur-md"}>
-              <div className={"flex justify-center"}>
-                  <h1 className={"text-fuchsia-400 title font-kanit text-4xl font-thin"}>Task Manager</h1>
-              </div>
-              <form className={"flex flex-col justify-center p-10 w-full gap-4"}>
-                  <CssTextField label="Email" type={"email"} variant="standard" />
-                  <CssTextField label="Password" type={"password"} variant="standard" />
-              </form>
-          </div>
-      </div>
+
+    async function SearchUser(event: FormEvent) {
+        event.preventDefault()
+        await api.post("/usuarios/login",{
+            email: user.email,
+            password: user.password
+        }).then(result => console.log(result))
+    }
+
+    return (
+        <div ref={myRef} className="h-[100vh] w-[100vw]">
+            <div
+                className={"flex flex-col justify-center card h-[100vh] w-[25vw] bg-violet-400 bg-opacity-10 backdrop-blur-md"}>
+                <div className={"flex justify-center"}>
+                    <h1 className={"text-fuchsia-500 font-kanit text-4xl font-thin"}>Task Manager</h1>
+                </div>
+                <form onSubmit={SearchUser} className={"flex flex-col justify-center p-10 w-full gap-6"}>
+                    <TextField sx={{
+                        "& label.Mui-focused": {
+                            color: "white",
+                        },
+                        "& .MuiInput-underline:before": {
+                            borderBottomColor: "white",
+                        },
+                        "& .MuiInput-underline:after": {
+                            borderBottomColor: "white",
+                        },
+                        //hover
+                        "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+                            borderBottomColor: "white",
+                        },
+                        "& .MuiFormLabel-root": {
+                            color: "white",
+                            fontFamily: "'Poppins', sans-serif",
+                            fontSize: "1rem",
+                        },
+                        "& .MuiInputBase-input": {
+                            color: "white",
+                            fontFamily: "'Poppins', sans-serif",
+                        },
+                    }} color={"secondary"} autoComplete={'off'} defaultValue={user.email} onChange={(e) => setUser({email: e.target.value, password: user.password})} label="Email" type={"email"} variant="standard"/>
+                    <TextField sx={{
+                        "& label.Mui-focused": {
+                            color: "white",
+                        },
+                        "& .MuiInput-underline:before": {
+                            borderBottomColor: "white",
+                        },
+                        "& .MuiInput-underline:after": {
+                            borderBottomColor: "white",
+                        },
+                        //hover
+                        "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+                            borderBottomColor: "white",
+                        },
+                        "& .MuiFormLabel-root": {
+                            color: "white",
+                            fontFamily: "'Poppins', sans-serif",
+                            fontSize: "1rem",
+                        },
+                        "& .MuiInputBase-input": {
+                            color: "white",
+                            fontFamily: "'Poppins', sans-serif",
+                        },
+                    }} autoComplete={'off'} defaultValue={user.password} onChange={(e) => setUser({password: e.target.value, email: user.email})} label="Password" type={"password"} variant="standard"/>
+                    <Button className={"bg-fuchsia-500 hover:bg-fuchsia-600"} variant="contained" type={"submit"}>Login</Button>
+                </form>
+            </div>
+        </div>
 
 
-  )
+    )
 }
